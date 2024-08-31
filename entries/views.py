17600ea78c177e38db0b6620eed0054d8fb2b034
nozomi <x8ojo8x@gmail.com>
2024-08-31 15:05:16 +0900

    First commit. あとフロントエンドの部分

---
+++ entries/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views.generic import (
+    ListView,
+    DetailView,
+)
+
+from .models import Entry
+
+class EntryListView(ListView):
+    model = Entry
+    queryset = Entry.objects.all().order_by("-date_created")
+
+class EntryDetailView(DetailView):
+    model = Entry
